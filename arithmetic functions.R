eb4efdddcccf374cd6b0892280884eedf8403bfa
aditisharma4747 <85553079+aditisharma4747@users.noreply.github.com>
2021-06-25 22:37:39 +0530

    Update arithmetic functions.R

--- arithmetic functions.R
+++ arithmetic functions.R
@@ -5,6 +5,7 @@
 66^2
 66^3
 sqrt(15)
+
 # RIGHT WAY TO FIND MEAN
 mean (22,33,44)
 c(22,33,44)
@@ -30,27 +31,27 @@ min(b)
 c = 1
 
 a = 1
+
+
 # storing values in c as 1
 c = 1
-# storing values in c as 1
-c = 1
+
 # storing values in a file
 a <-c (22,33,44)
 a = c(1,2,3,45,67)
-# to find out what is the in a number
-str(a)
+
 # to find out what is the in a number
 str(a)
 class(a)
 typeof(a)
+
 # to represent in the form of string
-b =
-  # to represent in the form of string
-  b = "Aditi"
+ b = "Aditi"
 str(b)
 class(b)
 typeof(b)
 b
+
 # series of value
 a <- 1:3
 # series of value
@@ -62,6 +63,7 @@ str(d)
 mean(d)
 median(d)
 class(d)
+
 # more irregular series
 e = c(1,4,6,8,50)
 e
@@ -72,32 +74,43 @@ e[c(3, 7)]
 e[c(3,7)]
 class(e)
 str(e)
+
 # more irregular series
 e = c(1,4,6,8,50,55,44)
+
 # more irregular series
 e = c(1,4,6,8,50,55,44)
+
 # more irregular series
 e = c(1,4,6,8,50,55,44)
 e
+
 e[3]  #gives 3rd number
 e[5]
 e[3:5] # gives us the value between 3rd to 5th
 e[c(3,7)]
 class(e)
 str(e)
+
 e[c(1,6)] #alternative method to get values from data
+
 e[c(1,6)] # alternative method to get  values from data
+
 # to convert integers as numbers
 c(1,3, 455,67,890, .654 ,6.77 , 34.65)
+
 #to round off
 round (c(1,3,455, 5.674 ,5.66, 67))
 abs = c(1,3,455, 5.674 ,5.66, 67)
 str(round (abs))
+
 #to round off
 round(c(1,3,455,5.674 ,5.66, 67))
 abs -c(1, 22, 3.55 , 66.56)
+
 #to round off
 round(c(1,3,455,5.674,5.66,67))
+
 #to round off
 round(c(1,3, 455,67,890, .654 ,6.77,34.65))
 abs = c(1, 22, 3.55,66.56)
@@ -131,6 +144,7 @@ names = c("aditi","palak","orbit")
 names{3}
 names = as.numeric
 names = as.numeric(n)
+
 # let's create a list
 as.list(c(name))
 # let's create a list
